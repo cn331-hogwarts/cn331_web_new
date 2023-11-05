536aed475f469b329fb3868cdb1e4cd885acb746
6410685074 <6410685074@student.tu.ac.th>
2023-11-05 14:04:08 +0700

    yoo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact component={Home} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+
+function Home() {
+  return (
+    <>
+    <h1>hello</h1>
+    </>
+  );
+}
+
+export default Home;
