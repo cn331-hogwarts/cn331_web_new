85c892e624ff2b500cb56b9633a3e247547a505d
6410685199 <88651079+6410685199@users.noreply.github.com>
2023-11-12 20:32:17 +0700

    frontend

--- frontend/src/components/pages/Signin.jsx
+++ frontend/src/components/pages/Signin.jsx
@@ -99,53 +99,50 @@ function update_form_btn() {
     )
   }
   return (
-    <div className="big-yo">
-      <h1 className="yo">hello</h1>
-      <Button id='form_btn' onClick={update_form_btn} >Register</Button>
-      {
-      registrationToggle ? (
-        <div className="center">
-          <Form onSubmit={e => submitRegistration(e)}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
-              <Form.Text className="text-muted">
-              </Form.Text>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicUsername">
-              <Form.Label>Username</Form.Label>
-              <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              submit_regis
-            </Button>
-          </Form>
-        </div>        
-      ) : (
-        <div className="center">
-          <Form onSubmit={e => submitLogin(e)}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
-              <Form.Text className="text-muted">
-              </Form.Text>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              login
-            </Button>
-          </Form>
+     <div className="body-container">
+        <div className="Signin-container">
+          <h1 className="yo">Sign in or Sign up</h1>
+          <Button id='form_btn' onClick={update_form_btn} >Register</Button>
+          {
+          registrationToggle ? (
+            <div className="register">
+              <Form onSubmit={e => submitRegistration(e)}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
+                  <Form.Text className="text-muted">
+                  </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicUsername">
+                  <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Sing up
+                </Button>
+              </Form>
+            </div>        
+          ) : (
+            <div className="login">
+              <Form onSubmit={e => submitLogin(e)}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
+                  <Form.Text className="text-muted">
+                  </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Sign in
+                </Button>
+              </Form>
+            </div>
+          )
+        }
         </div>
-      )
-    }
-    </div>
+      </div> 
   )
 
 }
