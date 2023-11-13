ac92c8972944f2f40daed5660032ef93bf35351a
6410685074 <6410685074@student.tu.ac.th>
2023-11-13 20:46:00 +0700

    merge about.jsx

--- backend/findaway/models.py
+++ backend/findaway/models.py
@@ -11,7 +11,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
     date_joined =  models.DateTimeField(auto_now_add=True)
-
+    
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["first_name", "last_name"]
 
--- frontend/src/components/pages/About.jsx
+++ frontend/src/components/pages/About.jsx
@@ -1,38 +1,58 @@
-import React from 'react';
+import React from 'react'
 import '../../App.css'
-import '../About.css';
-import axios from 'axios';
+import '../About.css'
 
 
-function About(){
+function About() {
     return (
-        <div className='about-container'>
-            <h1 className='about-title'>about</h1>
-            <p>
-                Purpose: matching perfect couple by using Ai to analyze their profile data and preferences<br />
-                Problem: for people who struggle to find meaningful connections and potential partners<br />
-                Improve: we will improve the matching system of the app<br />
-                Vision: users will trust our platform to find their perfect match and continue using our app to nurture and sustain their relationships.
-            </p>
-            <h1> members </h1>
-            <p>
-                - Kun Kerdthaisong 6410685074<br />
-                - Kittisak Suddaen 6410685082<br />
-                - Prin Yimrungruang 6410685199<br />
-                - Thanakorn Praimanee 6410685157<br />
-                - Suwijak Tantamaroj 6410685256
-            </p>
-            <h1>github repos</h1>
-                <a href='https://github.com/cn331-hogwarts/cn331_web_new'> web repo</a>
-                <br />
-                <a href='https://github.com/cn331-hogwarts/cn331_web_ai'> ai repo</a>
-                <br />
-                <a href='https://github.com/cn331-hogwarts/cn331_updates'> updates repo</a>
-                <br />
-                <a href='https://github.com/cn331-hogwarts'> project repo</a>
-            <h1>tracker</h1>
-                <a href='https://www.pivotaltracker.com/projects/2681787'> pivotaltracker link</a>
+        <div className='body-info'>
+            <div className='about-box'>
+                <div className='about-container'>
+                    <h1 className='about-title'>about</h1>
+                    <p className='about-paragraph'>
+                        Purpose: matching perfect couple by using Ai to analyze their profile data and preferences<br />
+                        Problem: for people who struggle to find meaningful connections and potential partners<br />
+                        Improve: we will improve the matching system of the app<br />
+                        Vision: users will trust our platform to find their perfect match and continue using our app to nurture and sustain their relationships.
+                    </p>
+                </div>
+            </div>
+            <div className='about-box'>
+                <div className='about-container'>
+                    <h1 className='about-title'>members</h1>
+                    <p className='about-paragraph'>
+                        - Kun Kerdthaisong 6410685074<br />
+                        - Kittisak Suddaen 6410685082<br />
+                        - Prin Yimrungruang 6410685199<br />
+                        - Thanakorn Praimanee 6410685157<br />
+                        - Suwijak Tantamaroj 6410685256
+                    </p>
+                </div>
+            </div>
+            <div className='about-box'>
+                <div className='about-container'>
+                    <h1 className='about-title'>github repos</h1>
+                    <p className='about-paragraph'>
+                        <a href='https://github.com/cn331-hogwarts/cn331_web_new'> web repo</a>
+                        <br />
+                        <a href='https://github.com/cn331-hogwarts/cn331_web_ai'> ai repo</a>
+                        <br />
+                        <a href='https://github.com/cn331-hogwarts/cn331_updates'> updates repo</a>
+                        <br />
+                        <a href='https://github.com/cn331-hogwarts'> project repo</a>
+                    </p>
+                </div>
+            </div>
+            <div className='about-box'>
+                <div className='about-container'>
+                    <h1 className='about-title'>tracker</h1>
+                    <p className='about-paragraph'>
+                        <a href='https://www.pivotaltracker.com/projects/2681787'> pivotaltracker link</a>
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
+
 export default About;
