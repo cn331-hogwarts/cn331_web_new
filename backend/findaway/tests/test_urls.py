c7378c9f2ed97a2ae7bcc58d3e8b91adcfbf2cfb
6410685157 <6410685157@student.tu.ac.th>
2023-12-02 21:36:14 +0700

    add test backend

---
+++ backend/findaway/tests/test_urls.py
@@ -0,0 +1,7 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+# class TestUrls(SimpleTestCase):
+
+#     def test_list_url_is_resolved(self):
+#         url = reverse()
