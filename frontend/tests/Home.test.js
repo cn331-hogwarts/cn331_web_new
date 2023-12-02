acf2f658b8012470788c149ae3ff60bbfe01eada
6410685157 <6410685157@student.tu.ac.th>
2023-12-02 21:33:33 +0700

    add Home.test.js

--- frontend/tests/Home.test.js
+++ frontend/tests/Home.test.js
@@ -3,9 +3,7 @@ import { render } from '@testing-library/react';
 import Home from '../src/components/pages/Home';
 
 test('renders home page correctly', () => {
-  // Render the Home component
   const { asFragment } = render(<Home />);
 
-  // Use snapshot testing to compare the rendered output
   expect(asFragment()).toMatchSnapshot();
 });
